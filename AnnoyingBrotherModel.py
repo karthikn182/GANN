36884dbdc2a92e466268cdc1828603857b766fc8
Austin Slakey <austin.slakey@columbia.edu>
2016-11-18 12:31:01 -0500

    Discriminator is Working!

--- AnnoyingBrotherModel.py
+++ AnnoyingBrotherModel.py
@@ -1,13 +1,13 @@
 
 
 #Things to-do
-#1. make output of Generator a sequence (not probabilties!)
-#2. Figure out concatenating in the right manner!
-#3. make sure train() function is correct
+#1. Discriminator seems to be working well
+#2. What is up with geerator though.  Debug outputs of encoder/decoder??
 
 import time
 import numpy as np
 import tensorflow as tf
+import os
 
 class AnnoyingBrotherConfig(object):
     """Define HyperParameters"""
@@ -18,7 +18,7 @@ class AnnoyingBrotherConfig(object):
     sequence_length = 5 
     hidden_size = 50 #size of one lstm cell
     memory_dim = 100 #memory dimension for GRU cell in seq2seq
-    max_epoch = 2
+    max_epoch = 1
     keep_prob = 0.5 #for dropout, Not Implemented
     lr_decay = 0.8
     batch_size = 1
@@ -73,29 +73,21 @@ class ABModel(object):
             #make prediction based on last output
             #d_output_vocab = 1 (high probability means it looks like real data)
             self.dweight = tf.Variable(tf.truncated_normal([num_hidden, self.d_output_vocab]))
+            self.dweight = tf.Print(self.dweight, [self.dweight], "D-weight: ")
             self.dbias = tf.Variable(tf.constant(0.1, shape=[self.d_output_vocab]))
+            self.dbias = tf.Print(self.dbias, [self.dbias], "D-bias: ")
+            #lets try cross entropy with logits
             self.prediction = tf.nn.softmax(tf.matmul(self.last, self.dweight) + self.dbias)
+            self.prediction = tf.Print(self.prediction, [self.prediction], "D-Predict: ")
 
             return self.prediction
 
         def generator(enc_inp):
-            #print(len(enc_inp))
-            #print(tf.shape_n(enc_inp))
-            #this one still needs work!
-            #input is encoder_input (a list of inputs)
-            #output is ?argmax? of decoder_output
 
             print("generator architecture")
 
             #Lets change this to be embedding rnn model a la
             #https://github.com/hans/ipython-notebooks/blob/master/tf/TF%20tutorial.ipynb
-
-            #self.labels = [tf.placeholder(tf.int32, shape=(None,),
-             #                       name="labels%i" % t)
-              #        for t in range(self.seq_length)]
-
-            #self.weights = [tf.ones_like(labels_t, dtype=tf.float32)
-             #          for labels_t in self.labels]
 
             # Decoder input: prepend some "GO" token and drop the final
             # token of the encoder input
@@ -123,8 +115,9 @@ class ABModel(object):
             self.d_out, self.dec_memory = tf.nn.seq2seq.embedding_rnn_seq2seq(
                 self.enc_inp, self.dec_inp, self.cell, self.input_vocab_size, self.output_vocab_size,self.memory_dim)
 
-            #might need to take softargmax? AND pack it back into one tensor!
-            self.g_out = self.pack_sequence(self.d_out)
+            #might need to take softmax? AND pack it back into one tensor!
+            self.packed = self.pack_sequence(self.d_out)
+            self.g_out = self.softmax(self.packed, 2) #target, axis
             print("Generator out shape:",self.g_out.get_shape())
             return self.g_out
 
@@ -159,10 +152,20 @@ class ABModel(object):
             #how to combine question and G_out? Input concatenation of qd,Gz
             self.Dg = discriminator(self.Gz) #Produces probabilities for generated question/answer pair
 
+        #Trying cross entropy loss on target (None,2)
+        self.real_target = tf.placeholder(shape=(None,2),dtype=tf.float32)
+        self.gen_target = tf.placeholder(shape=(None,2),dtype=tf.float32)
+        print("target",self.real_target.get_shape())
+        print("prediction",self.Dx.get_shape())
+
+        self.d_cost_real = -tf.reduce_sum(self.real_target * tf.log(self.Dx + 1e-50))
+        self.d_cost_gen = -tf.reduce_sum(self.gen_target * tf.log(self.Dg + 1e-50))
+        self.d_loss = self.d_cost_real + self.d_cost_gen
+        self.g_loss = -tf.reduce_sum(self.real_target * tf.log(self.Dg + 1e-50))
 
         #These functions together define the optimization objective of the GAN.
-        self.d_loss = -tf.reduce_mean(tf.log(self.Dx) + tf.log(1.-self.Dg)) #This optimizes the discriminator.
-        self.g_loss = -tf.reduce_mean(tf.log(self.Dg)) #This optimizes the generator.
+        #self.d_loss = -tf.reduce_mean(tf.log(self.Dx + 1e-50) + tf.log(1.-self.Dg)) #This optimizes the discriminator.
+        #self.g_loss = -tf.reduce_mean(tf.log(self.Dg + 1e-50)) #This optimizes the generator.
 
 
 
@@ -174,16 +177,16 @@ class ABModel(object):
         print("tvars length: ",len(self.tvars)) #length 18
 
         print("about to comput gradients")
-        self.d_grads = self.trainerD.compute_gradients(self.d_loss,self.tvars[9:]) #Only update the weights for the discriminator network.
-        self.g_grads = self.trainerG.compute_gradients(self.g_loss,self.tvars[0:9])
+        #self.d_grads = self.trainerD.compute_gradients(self.d_loss,self.tvars[9:]) #Only update the weights for the discriminator network.
+        #self.g_grads = self.trainerG.compute_gradients(self.g_loss,self.tvars[0:9])
         #print("computing d gradients",self.d_grads)
         #print("computing g gradients",self.g_grads)
 
 
-        #d_params = [v for v in tvars if v.name.startswith('D')]
-        #g_params = [v for v in tvars if v.name.startswith('G')]
-        #d_grads = trainerD.compute_gradients(d_loss,d_params) #Only update the weights for the discriminator network.
-        #g_grads = trainerG.compute_gradients(g_loss,g_params) #Only update the weights for the generator network.
+        self.d_params = [v for v in self.tvars if v.name.startswith('D')]
+        self.g_params = [v for v in self.tvars if v.name.startswith('G')]
+        self.d_grads = self.trainerD.compute_gradients(self.d_loss,self.d_params) #Only update the weights for the discriminator network.
+        self.g_grads = self.trainerG.compute_gradients(self.g_loss,self.g_params) #Only update the weights for the generator network.
 
         self.update_D = self.trainerD.apply_gradients(self.d_grads)
         self.update_G = self.trainerG.apply_gradients(self.g_grads)
@@ -194,6 +197,11 @@ class ABModel(object):
     #TRAINING
     def train(self,data):
         print("training")
+        #hardcoding these targets
+        real_t = np.ones(shape=(1,2))
+        real_t[0][1] = 0
+        gen_t= np.ones(shape=(1,2))
+        gen_t[0][0] = 0
 
         batch_size = self.batch_size #Size of image batch to apply at each iteration.
         epochs = self.max_epoch
@@ -207,15 +215,15 @@ class ABModel(object):
             sess.run(init)
             for _ in range(epochs):
                 index = 0
-                for i in range(iterations):
+                for i in range(10):
                     #grab integer question from data for generator
                     qg = data[index:index+batch_size]
-                    print("qg",qg)
+                    #print("qg",qg)
                     index += batch_size
 
                     #grab real answer from question, resized for discriminator (batch,seq_length,features)
                     answer = self.RealAnswer(qg)
-                    print("answer",answer)
+                    #print("answer",answer)
 
                     '''
                     Just keeping this in here for now...
@@ -225,34 +233,40 @@ class ABModel(object):
                     xs = (np.reshape(xs,[batch_size,28,28,1]) - 0.5) * 2.0 #Transform it to be between -1 and 1
                     xs = np.lib.pad(xs, ((0,0),(2,2),(2,2),(0,0)),'constant', constant_values=(-1, -1)) #Pad the images so the are 32x32
                     '''
+                    #feeding in encoder input and real target (want discriminator to think this is real!)
                     #feed_dict = {enc_inp[t]: X[t] for t in range(seq_length)}
                     #sorry hardcoding the batch size one here  :/
                     gfeed_dict = {self.enc_inp[t]: [qg[0][t]] for t in range(self.seq_length)}
+                    gfeed_dict.update({self.real_target: real_t})
+
                     dfeed_dict = {self.enc_inp[t]: [qg[0][t]] for t in range(self.seq_length)}
                     dfeed_dict.update({self.real_in: answer})
+                    dfeed_dict.update({self.real_target: real_t})
+                    dfeed_dict.update({self.gen_target: gen_t})
 
 
                     _,dLoss = sess.run([self.update_D,self.d_loss],dfeed_dict)#Update the discriminator
-                    _,gLoss = sess.run([self.update_G,self.g_loss],gfeed_dict) #update generator
+                    #_,gLoss = sess.run([self.update_G,self.g_loss],gfeed_dict) #update generator
 
-                    if i % 100 == 0:
-                        #print a question/Answer
-                        print("Gen Loss: " + str(gLoss) + " Disc Loss: " + str(dLoss))
-                        q2 = data[0:0+batch_size] #using the first batch of question to see how we improve
-                        newA = sess.run(self.Gz,feed_dict={self.enc_inp: [q2[0][t]] for t in range(self.seq_length)})
+                    if i % 2 == 0:
+                        #print("Gen Loss: " + str(gLoss) + " Disc Loss: " + str(dLoss))
+                        print("Disc Loss: "+str(dLoss))
+                        newA = sess.run(self.Gz,feed_dict={self.enc_inp[t]: [qg[0][t]] for t in range(self.seq_length)})
 
                         if not os.path.exists(sample_directory):
                             os.makedirs(sample_directory)
                         #Save sample generator images for viewing training progress.
-                        save_Answer(q2,newA,sample_directory+'/fig'+str(i))
-                    if i % 1000 == 0 and i != 0:
+                        #print("question:",qg)
+                        #print("generated Answer:",newA)
+                        self.save_Answer(qg,newA,sample_directory+'/fig'+str(i))
+                    if i % 500 == 0 and i != 0:
                         if not os.path.exists(model_directory):
                             os.makedirs(model_directory)
                         saver.save(sess,model_directory+'/model-'+str(i)+'.cptk')
                         print("Saved Model")
 
 
-
+#--------------------Support Functions------------------------------------------#
     def RealAnswer(self, q_in):
         #Contains Logic to make the real answer from the question
         #for now, just adding 2 to every word (annoying right?)
@@ -268,9 +282,16 @@ class ABModel(object):
         #z[list(np.indices(z.shape[:-1])) + [a]] = 1
         return z
 
-        def save_answer(self, q, a, path):
-            answer = np.concatenate((q,a),axis=1)
-            return np.savetxt(path,answer)
+    def save_Answer(self, q, a, path):
+        print("saving answer")
+        print(q)
+        print(a)
+        answer = np.argmax(a, axis=2).flatten()
+        question = q.flatten()
+        print("qesution",question)
+        print("anser",answer)
+        out = np.append(q,a)
+        return out
 
 
     #Extremely useful for transfering from seq2seq stuff to the dynamic rnn classification
@@ -285,4 +306,17 @@ class ABModel(object):
         """Combine a list of the frames into a single tensor of the sequence."""
         #return tf.transpose(tf.pack(sequence), perm=[1, 0, 2])
         return tf.pack(sequence,axis=1)
+    """
+    Multi dimensional softmax,
+    refer to https://github.com/tensorflow/tensorflow/issues/210
+    compute softmax along the dimension of target
+    the native softmax only supports batch_size x dimension
+    """
+    def softmax(self, target, axis, name=None):
+      #with tf.name_scope(name, 'softmax', values=[target]):
+        max_axis = tf.reduce_max(target, axis, keep_dims=True)
+        target_exp = tf.exp(target-max_axis)
+        normalize = tf.reduce_sum(target_exp, axis, keep_dims=True)
+        softmax = target_exp / normalize
+        return softmax
 
